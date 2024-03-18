1cd35b468b12ea27fcc0216787afd3cd27bb127a
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.brC:/Users/Pedro/AppData/Local/Programs/Python/Python38/python.exe>
2024-03-18 13:36:16 -0300

    first commit

---
+++ demo/src/main/java/com/orbitaltech/demo/OrbitaltechApplication.java
@@ -0,0 +1,13 @@
+package com.orbitaltech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitaltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitaltechApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/orbitaltech/demo/OrbitaltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbitaltech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitaltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
