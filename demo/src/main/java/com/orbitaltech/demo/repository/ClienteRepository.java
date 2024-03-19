8aff6d24c55f6db5938fb0d196439c8249419391
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.brC:/Users/Pedro/AppData/Local/Programs/Python/Python38/python.exe>
2024-03-19 11:51:39 -0300

    criando a controller e a repository

--- demo/src/main/java/com/orbitaltech/demo/repository/ClienteRepository.java
+++ demo/src/main/java/com/orbitaltech/demo/repository/ClienteRepository.java
@@ -1,4 +1,8 @@
 package com.orbitaltech.demo.repository;
 
-public interface ClienteRepository {
+import com.orbitaltech.demo.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
 }
