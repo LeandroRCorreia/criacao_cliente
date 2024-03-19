aa8c81e6b5e4ff19a7970e7dc047d66d83feba65
LeandroRCorreia <leandrohamlet@gmail.com>
2024-03-19 14:53:55 -0300

    Pegar id cliente por index + listar todos os clientes + deletar cliente especifico

--- demo/src/main/java/com/orbitaltech/demo/controller/ClienteController.java
+++ demo/src/main/java/com/orbitaltech/demo/controller/ClienteController.java
@@ -1,14 +1,48 @@
 package com.orbitaltech.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.orbitaltech.demo.model.Cliente;
+import com.orbitaltech.demo.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Console;
+import java.util.List;
 
 @RestController
 public class ClienteController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/clientes")
+    public List<Cliente> Clientes()
+    {
+        List<Cliente> clientesList = clienteRepository.findAll();
+
+        return clientesList;
+    }
+
+    @GetMapping("/cliente/{id}")
+    public Cliente Cliente(@PathVariable long id)
+    {
+        Cliente cliente = clienteRepository.
+                          findById(id).
+                          orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        return cliente;
+    }
+
+    @DeleteMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void Delete(@PathVariable long id)
+    {
+        Cliente cliente = clienteRepository.
+                findById(id).
+                orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        clienteRepository.deleteById(cliente.getId());
+
     }
 
 }
