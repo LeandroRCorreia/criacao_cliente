3a8a7dcc6a2871897a65c81a3730647502aa764d
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.brC:/Users/Pedro/AppData/Local/Programs/Python/Python38/python.exe>
2024-03-25 09:54:31 -0300

    modificando service para adicicao de endereco

--- demo/src/main/java/com/orbitaltech/demo/adapter/api/ConsultaApi.java
+++ demo/src/main/java/com/orbitaltech/demo/adapter/api/ConsultaApi.java
@@ -3,12 +3,13 @@ package com.orbitaltech.demo.adapter.api;
 import com.orbitaltech.demo.dto.ViaCepEnderecoDTO;
 import com.orbitaltech.demo.model.Endereco;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "apiCep", url = "https://viacep.com.br/ws")
 public interface ConsultaApi {
     @GetMapping("/{cep}/json/")
-    public ViaCepEnderecoDTO responseEndereco(@PathVariable("cep") String cep) ;
+     ResponseEntity<ViaCepEnderecoDTO> responseEndereco(@PathVariable("cep") String cep) ;
 
 }
--- demo/src/main/java/com/orbitaltech/demo/service/ClienteService.java
+++ demo/src/main/java/com/orbitaltech/demo/service/ClienteService.java
@@ -7,7 +7,13 @@ import com.orbitaltech.demo.model.Cliente;
 import com.orbitaltech.demo.model.Endereco;
 import com.orbitaltech.demo.repository.ClienteRepository;
 import com.orbitaltech.demo.repository.EnderecoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,25 +27,31 @@ public class ClienteService {
     @Autowired
     EnderecoRepository enderecoRepository;
 
+
     public List<Cliente> listarCliente() {
         return clienteRepository.findAll();
     }
 
+    @Transactional
     public void deletar(long id) {
         Cliente clienteParaSerDeletado = buscarOuFalhar(id);
-        clienteRepository.delete(clienteParaSerDeletado);
+        try {
+            clienteRepository.delete(clienteParaSerDeletado);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException();
+            // TODO fazer exceção específica para o delete
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException();
+            // TODO fazer exceção específica para o delete
+        }
+
     }
 
+    @Transactional
     public Cliente adicionar(ClienteInputDto clienteAdicionado) {
-        ViaCepEnderecoDTO cepEnderecoDTO = viaCepAdapter.responseEndereco(clienteAdicionado.getCep());
         Cliente cliente = new Cliente();
-        Endereco endereco = Endereco.builder()
-                .logradouro(cepEnderecoDTO.getLogradouro())
-                .cep(cepEnderecoDTO.getCep())
-                .cidade(cepEnderecoDTO.getLocalidade())
-                .uf(cepEnderecoDTO.getUf())
-                .build();
-        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+
+        Endereco enderecoSalvo = copiaEnderecoParaCliente(clienteAdicionado);
 
         cliente.setNome(clienteAdicionado.getNome());
         cliente.setDataNascimento(clienteAdicionado.getDataNascimento());
@@ -48,7 +60,9 @@ public class ClienteService {
         return clienteRepository.save(cliente);
     }
 
+    @Transactional
     public Cliente atualizar(long clienteId, Cliente clienteAtualizado) {
+
         Cliente clienteParaSerAtualizado = buscarOuFalhar(clienteId);
 
         clienteParaSerAtualizado.setNome(clienteAtualizado.getNome());
@@ -62,7 +76,39 @@ public class ClienteService {
         return clienteRepository.findById(clienteId).orElseThrow(
                 () -> new IllegalArgumentException("Cliente não encontrado"));
 
+        // TODO fazer exceção de cliente não encontrado
+
     }
+
+    private Endereco copiaEnderecoParaCliente(ClienteInputDto clienteAdicionado) {
+        ResponseEntity<ViaCepEnderecoDTO> cepEnderecoDTO = viaCepAdapter.responseEndereco(clienteAdicionado.getCep());
+
+        buscarOuFalharEndereco(cepEnderecoDTO);
+
+        Endereco endereco = Endereco.builder()
+                .logradouro(cepEnderecoDTO.getBody().getLogradouro())
+                .cep(cepEnderecoDTO.getBody().getCep())
+                .cidade(cepEnderecoDTO.getBody().getLocalidade())
+                .uf(cepEnderecoDTO.getBody().getUf())
+                .build();
+
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+        return enderecoSalvo;
+    }
+
+    private static void buscarOuFalharEndereco(ResponseEntity<ViaCepEnderecoDTO> cepEnderecoDTO) {
+        if (cepEnderecoDTO.getBody().isResponseError()) {
+            throw new RuntimeException();
+            // TODO fazer o retorno de endereco não encontrado
+        }
+        if (cepEnderecoDTO.getStatusCode().is4xxClientError()) {
+            throw new RuntimeException();
+            // TODO fazer o retorno de formato do endereço descrito está errado não encontrado
+        }
+    }
+
+
+
 }
 
 
